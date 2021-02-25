5c32580717ec53c1b5cc6ca768a34c3e16166850
Alfie Richardson <01richardsonalfie@gmail.com>
2021-02-25 18:37:13 +0000

    Added constructor

--- src/Intersection.java
+++ src/Intersection.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+
 public class Intersection {
 
-    public Intersection() {
+    public ArrayList<Street> inStreets;
+    public ArrayList<Street> outStreets;
+    public String schedule;
 
+    public Intersection(ArrayList<Street> inStreets, ArrayList<Street> outStreets) {
+        this.inStreets = inStreets;
+        this.outStreets = outStreets;
     }
 
 }
