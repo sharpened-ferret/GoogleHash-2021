9745b377dee95b9d84713a82a77801a980bb6f95
joshua <j.murray567@gmail.com>
2021-02-25 19:16:08 +0000

    File Writer

---
+++ src/FileWrite.java
@@ -0,0 +1,21 @@
+
+import java.io.File;  // Import the File class
+import java.io.FileWriter;
+import java.io.IOException;  // Import the IOException class to handle errors
+public class FileWrite {
+
+
+    public FileWrite(String solName, String data){
+        try {
+            FileWriter myWriter = new FileWriter(solName + "_solution.txt");
+            myWriter.write(data);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -38,6 +38,8 @@ public class Main {
             fSolution += aData.InterMap.get(i).toString();
         }
 
+        FileWrite fileWrite = new FileWrite("c",cSolution);
+
 
 
 
